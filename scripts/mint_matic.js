207e221352e7483c0420448596a2a51599354ac7
Jose Perez <mobilefl44@gmail.com>
2021-08-03 16:01:30 -0400

    Create mint_matic.js

---
+++ scripts/mint_matic.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat');
+async function main() {
+  const [signer] = await ethers.getSigners();
+  console.log('Signer address', signer.address);
+  /**
+   * Contract factory
+   */
+  const NFT = await hre.ethers.getContractFactory('FlatChildCreature4');
+  // const URI = 'something';
+
+  /**
+   * Owner of the nft and contract addresses
+   */
+  const WALLET_ADDRESS = '0x6844b555aed63c1741F469Ae87dE219a87825bFe';
+  const CONTRACT_ADDRESS = '0x89e1115e530958dFB62A46ad299e210F791AF257';// '0x5973103c2C119115236dCB81BC56250E5E86fF59';
+
+  /**
+   * Returns a new instance of the Contract attached to a new address
+   */
+  const contract = NFT.attach(CONTRACT_ADDRESS);
+
+  /**
+   * Minting a new NFT
+   */
+  const txResult = await contract.mintTo(WALLET_ADDRESS);
+  console.log('Tx Result:', txResult);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
